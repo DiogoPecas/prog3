7e784eb89d4aaf082eecec70b8262eb9a8e7b9af
DiogoPecas <aluno23789@ipt.pt>
2023-06-11 20:46:36 +0100

    ex13

---
+++ ProjectEx13/src/entities/employee.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class employee {
+	public String name;
+	public int hours;
+	public double value;
+	public double charge;
+	public boolean outsourced;
+
+	public employee(String name, int hours, double value, double charge, boolean outsourced) {
+		this.name = name;
+		this.hours = hours;
+		this.value = value;
+		this.charge = charge;
+		this.outsourced = outsourced;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	 public double calculatePayment() {
+	        double Payment = hours * value;
+	        double totalPayment = outsourced ? (Payment + charge) : Payment;
+	        return totalPayment;
+	 }
+	
+}
